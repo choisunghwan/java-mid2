c9d8d1cc8409936a6dfc3a03f0423b6dabcc4f89
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2025-06-08 16:46:27 +0900

    # 27. 직접 구현하는 배열 리스트3 - 기능 추가

---
+++ src/collection/MyArrayListV3.java
@@ -0,0 +1,109 @@
+package collection;
+
+import java.util.Arrays;
+
+public class MyArrayListV3 {
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementDate;
+    private int size = 0;
+
+    public MyArrayListV3(){
+        elementDate = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV3(int initialCapacity) {
+        elementDate = new Object[initialCapacity];
+    }
+    public int size() {
+        return size;
+    }
+
+    public void add(Object e) {
+        if (size == elementDate.length) {
+            grow();
+        }
+        elementDate[size] = e;
+        size++;
+    }
+
+    // 코드 추가
+    public void add(int index, Object e) {
+        if (size == elementDate.length) {
+            grow();
+        }
+
+        //데이터 이동
+        shiftRightFrom(index);
+        elementDate[index] = e;
+        size++;
+    }
+
+    // 코드 추가, 요소의 마지막부터 index까지 오른쪽으로 밀기
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementDate[i] = elementDate[i - 1];
+        }
+    }
+
+    //코드 추가
+    public Object remove(int index) {
+        Object oldValue = get(index);
+        //데이터 이동
+        shiftLeftFrom(index);
+
+        size--;
+        elementDate[size] = null;
+        return oldValue;
+
+    }
+
+    //코드 추가 요소의 index 부터 마지막 까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementDate[i] = elementDate[i + 1];
+        }
+    }
+
+    private void grow() {
+        int oldCapacity = elementDate.length;
+        int newCapacity = oldCapacity * 2;
+        elementDate = Arrays.copyOf(elementDate, newCapacity);
+        // 배열을 새로 만들고, 기존 배열을 새로운 배열에 복사
+/*        Object[] newArr = new Object[newCapacity];
+
+        for (int i = 0; i < elementDate.length; i++) {
+            newArr[i] = elementDate[i];
+        }*/
+
+        /*Object[] newArr = Arrays.copyOf(elementDate, newCapacity);
+        elementDate = newArr;*/
+    }
+
+
+    public Object get(int index) {
+        return elementDate[index];
+    }
+
+    public Object set(int index, Object element) {
+        Object oldValue = get(index);
+        elementDate[index] = element;
+        return oldValue;
+    }
+
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementDate[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public String toString() {
+        // [1,2,3,null,null] // size = 3
+        // [1,2,3] // size = 3
+//        return Arrays.toString(elementDate);
+        return Arrays.toString(Arrays.copyOf(elementDate, size)) +
+                "size = " + size + ", capacity = " + elementDate.length;
+    }
+}
---
+++ src/collection/MyArrayListV3Main.java
@@ -0,0 +1,29 @@
+package collection;
+
+public class MyArrayListV3Main {
+    public static void main(String[] args) {
+        MyArrayListV3 list = new MyArrayListV3();
+        // 마지막에 추가 //0(1)
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        // 원하는 위치에 추가
+        System.out.println("addLast");
+        list.add(3, "addLast");
+        System.out.println(list);
+
+        System.out.println("addFirst");
+        list.add(0, "addFirst");
+        System.out.println(list);
+
+        //삭제
+        Object removed1 = list.remove(4);
+        System.out.println("removed1 = " + removed1);
+
+        Object removed2 = list.remove(0);
+        System.out.println("removed2 = " + removed2);
+
+    }
+}
